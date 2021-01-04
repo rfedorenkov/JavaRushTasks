5010996cafd63bcd864b88ff2a66e8da1a85dfd0
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-01-04 10:49:59 +0300

    Lesson 9 Complete

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1117/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task11.task1117;
+
+/**
+ * Пакет нужен?
+ * Исправь ошибку в коде, чтобы программа компилировалась и работала.
+ *
+ *
+ * Требования:
+ * 1. Программа должна компилироваться и работать.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Test package");
+    }
+}
