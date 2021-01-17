7bdf806f3c788451864eb2da3781b93cd5d485ad
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-01-17 10:51:03 +0300

    Java Core level 1 complete

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1112/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task11.task1112;
+
+/**
+ * iPhone и Apple
+ * Измени два класса — Apple и IPhone.
+ * Унаследуй IPhone от Apple.
+ *
+ *
+ * Требования:
+ * 1. В классе Solution должен быть класс Apple.
+ * 2. Класс Apple должен быть public.
+ * 3. В классе Solution должен быть класс IPhone.
+ * 4. Класс IPhone должен быть public.
+ * 5. Класс IPhone должен быть унаследован от класса Apple.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Apple {
+
+    }
+
+    public class IPhone extends Apple {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task11/task1115/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task11.task1115;
+
+/**
+ * От школьника до пенсии
+ * Измени четыре класса: Schoolboy (школьник), Student (студент), Worker (Сотрудник), Retiree (Пенсионер).
+ * Унаследовать студента от школьника, сотрудника от студента, пенсионера от сотрудника.
+ *
+ *
+ * Требования:
+ * 1. В классе Solution должен быть public класс Schoolboy (школьник).
+ * 2. В классе Solution должен быть public класс Student (студент).
+ * 3. В классе Solution должен быть public класс Worker (Сотрудник).
+ * 4. В классе Solution должен быть public класс Retiree (Пенсионер).
+ * 5. Класс Student должен быть унаследован от класса Schoolboy.
+ * 6. Класс Worker должен быть унаследован от класса Student.
+ * 7. Класс Retiree должен быть унаследован от класса Worker.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Schoolboy {
+
+    }
+
+    public class Student extends Schoolboy {
+
+    }
+
+    public class Worker extends Student {
+
+    }
+
+    public class Retiree extends Worker {
+
+    }
+
+}
