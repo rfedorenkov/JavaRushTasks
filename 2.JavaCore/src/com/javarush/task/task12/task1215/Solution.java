72c9b8883620015b800253805e5b54ae67eaf9a0
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-01-20 10:15:29 +0300

    Java Core level 2 complete

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1215/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task12.task1215;
+
+/**
+ * Кошки не должны быть абстрактными!
+ * Унаследуй классы Cat и Dog от Pet.
+ * Реализуй недостающие методы. Классы Cat и Dog не должны быть абстрактными.
+ *
+ *
+ * Требования:
+ * 1. Класс Pet должен быть абстрактным.
+ * 2. Класс Dog не должен быть абстрактным.
+ * 3. Класс Cat не должен быть абстрактным.
+ * 4. Класс Dog должен наследоваться от класса Pet и реализовывать его абстрактные методы.
+ * 5. Класс Cat должен наследоваться от класса Pet и реализовывать его абстрактные методы.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public abstract String getName();
+
+        public abstract Pet getChild();
+    }
+
+    public static class Cat extends Pet {
+
+        @Override
+        public String getName() {
+            return "Кошка";
+        }
+
+        @Override
+        public Pet getChild() {
+            return new Cat();
+        }
+    }
+
+    public static class Dog extends Pet {
+
+        @Override
+        public String getName() {
+            return "Собака";
+        }
+
+        @Override
+        public Pet getChild() {
+            return new Dog();
+        }
+    }
+
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1216/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task12.task1216;
+
+/**
+ * Летать охота
+ * Напиши свой public интерфейс CanFly(летать).
+ * Добавь в него два метода.
+ *
+ *
+ * Требования:
+ * 1. Класс Solution должен содержать интерфейс CanFly.
+ * 2. Интерфейс CanFly должен быть публичным.
+ * 3. Интерфейс CanFly должен содержать два метода.
+ * 4. У интерфейса CanFly не должно быть родителя.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void fly();
+        boolean canFly();
+    }
+
+}
