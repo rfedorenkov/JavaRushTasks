dec5bfdd33c35876b47b9c15bacff62250922ea6
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-02-01 11:20:47 +0300

    Java Core level 5 complete

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1609/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task16.task1609;
+
+/**
+ * Справедливость
+ * 1. Разберись, что делает программа.
+ * 2. Нужно сделать так, чтобы все мыши ели одновременно.
+ * 3. Подумай, какой метод позволяет альфа-самцу мыши есть первым, и почему остальные мыши ждут.
+ * 4. Удали вызов этого метода.
+ *
+ *
+ * Требования:
+ * 1. Вывод программы должен показывать, что сначала все мыши начали есть, а потом все закончили.
+ * 2. Метод join не должен вызываться у объектов типа Mouse.
+ * 3. Программа должна создавать 3 объекта типа Mouse.
+ * 4. Методы, которые отвечают за вывод в консоль, не изменять.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Mouse alpha = new Mouse("#1");
+        Mouse mouse1 = new Mouse("#2");
+        Mouse mouse2 = new Mouse("#3");
+    }
+
+    private static void eat() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    public static class Mouse extends Thread {
+        public Mouse(String name) {
+            super(name);
+            start();
+        }
+
+        @Override
+        public void run() {
+            System.out.println(getName() + " is starting to eat");
+            eat();
+            System.out.println(getName() + " has finished eating");
+        }
+    }
+}
