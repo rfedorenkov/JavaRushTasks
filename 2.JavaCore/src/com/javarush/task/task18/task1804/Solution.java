d26a8af15ff4b2f54fe80056ad898534cfc66dd7
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-02-06 16:26:03 +0300

    Java Core level 8 complete

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -1,10 +1,7 @@
 package com.javarush.task.task18.task1804;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
