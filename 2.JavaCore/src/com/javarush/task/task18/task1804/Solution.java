de5b77692a36bb4a938ee47a07d9ffbc595fd3c0
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-02-06 16:04:30 +0300

    Java Core level 8 complete

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -1,12 +1,16 @@
-package com.javarush.task.task18.task1803;
+package com.javarush.task.task18.task1804;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
- * Самые частые байты
+ * Самые редкие байты
  * Ввести с консоли имя файла.
- * Найти байт или байты с максимальным количеством повторов.
+ * Найти байт или байты с минимальным количеством повторов.
  * Вывести их на экран через пробел.
  * Закрыть поток ввода-вывода.
  *
@@ -14,7 +18,7 @@ import java.util.*;
  * Требования:
  * 1. Программа должна считывать имя файла с консоли.
  * 2. Для чтения из файла используй поток FileInputStream.
- * 3. В консоль через пробел должны выводиться все байты из файла с максимальным количеством повторов.
+ * 3. В консоль через пробел должны выводиться все байты из файла с минимальным количеством повторов.
  * 4. Данные в консоль должны выводится в одну строку.
  * 5. Поток чтения из файла должен быть закрыт.
 */
@@ -35,17 +39,18 @@ public class Solution {
             e.printStackTrace();
         }
 
-        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         for (int b : bytes) {
-            if (b > max) {
-                max = b;
+            if (b > 0 && min > b) {
+                min = b;
             }
         }
 
         for (int i = 0; i < bytes.length; i++) {
-            if (bytes[i] == max) {
+            if (min == bytes[i]) {
                 System.out.print(i + " ");
             }
+
         }
     }
 }
