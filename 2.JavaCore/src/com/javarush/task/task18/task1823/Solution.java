1c1c203ded989d404e00ca7d0b2a9ab7764bc7c5
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-03-05 09:06:31 +0300

    Update

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task18.task1823;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Нити и байты
+ * Читайте с консоли имена файлов, пока не будет введено слово "exit".
+ * Передайте имя файла в нить ReadThread.
+ * Нить ReadThread должна найти байт, который встречается в файле максимальное число раз,
+ * и добавить его в словарь resultMap,
+ * где параметр String - это имя файла, параметр Integer - это искомый байт.
+ * Закрыть потоки.
+ *
+ *
+ * Требования:
+ * 1. Программа должна считывать имена файлов с консоли, пока не будет введено слово "exit".
+ * 2. Для каждого файла создай нить ReadThread и запусти ее.
+ * 3. После запуска каждая нить ReadThread должна создать свой поток для чтения из файла.
+ * 4. Затем нити должны найти максимально встречающийся байт в своем файле и добавить его в словарь resultMap.
+ * 5. Поток для чтения из файла в каждой нити должен быть закрыт.
+*/
+
+public class Solution {
+    public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String readLine;
+        while ((readLine = scanner.next()).equals("exit")) {
+            new ReadThread(readLine).start();
+        }
+    }
+
+    public static class ReadThread extends Thread {
+        private final String fileName;
+
+        public ReadThread(String fileName) {
+            this.fileName = fileName;
+        }
+        @Override
+        public void run() {
+            int[] bytes = new int[256];
+
+            try (FileInputStream fis = new FileInputStream(fileName)) {
+                while (fis.available() > 0) {
+                    int read = fis.read();
+                    bytes[read]++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int maxCount = 0;
+            int maxCountByte = 0;
+
+            for (int i = 0; i < bytes.length; i++) {
+                if (bytes[i] > maxCount) {
+                    maxCount = bytes[i];
+                    maxCountByte = i;
+                }
+            }
+
+            resultMap.put(fileName, maxCountByte);
+        }
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -0,0 +1,101 @@
+package com.javarush.task.task19.task1903;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Адаптация нескольких интерфейсов
+ * Адаптируй IncomeData к Customer и Contact.
+ * Классом-адаптером является IncomeDataAdapter.
+ * Инициализируй countries перед началом выполнения программы. Соответствие кода страны и названия:
+ * UA Ukraine
+ * RU Russia
+ * CA Canada
+ * Дополнить телефонный номер нулями до 10 цифр при необходимости (смотри примеры).
+ * Обратите внимание на формат вывода фамилии и имени человека.
+ *
+ *
+ * Требования:
+ * 1. Класс Solution должен содержать public static поле countries типа Map<String, String>.
+ * 2. В статическом блоке класса Solution инициализируй поле countries тестовыми данными согласно заданию.
+ * 3. Класс IncomeDataAdapter должен реализовывать интерфейсы Customer и Contact.
+ * 4. Класс IncomeDataAdapter должен содержать приватное поле data типа IncomeData.
+ * 5. Класс IncomeDataAdapter должен содержать конструктор с параметром IncomeData.
+ * 6. В классе IncomeDataAdapter реализуй методы интерфейсов Customer и Contact используя подсказки
+ * в виде комментариев в интерфейсах.
+ */
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<>();
+
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class IncomeDataAdapter implements Customer, Contact {
+
+        private final IncomeData data;
+
+        public IncomeDataAdapter(IncomeData data) {
+            this.data = data;
+        }
+
+        @Override
+        public String getCompanyName() {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(data.getCountryCode());
+        }
+
+        @Override
+        public String getName() {
+            return String.format("%s, %s", data.getContactLastName(), data.getContactFirstName());
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            String phoneNumber = String.format("%010d", data.getPhoneNumber());
+            return String.format("+%d(%s)%s-%s-%s", data.getCountryPhoneCode(),
+                    phoneNumber.substring(0,3),
+                    phoneNumber.substring(3,6),
+                    phoneNumber.substring(6,8),
+                    phoneNumber.substring(8,10));
+        }
+    }
+
+
+    public interface IncomeData {
+        String getCountryCode();        //For example: UA
+
+        String getCompany();            //For example: JavaRush Ltd.
+
+        String getContactFirstName();   //For example: Ivan
+
+        String getContactLastName();    //For example: Ivanov
+
+        int getCountryPhoneCode();      //For example: 38
+
+        int getPhoneNumber();           //For example: 501234567
+    }
+
+    public interface Customer {
+        String getCompanyName();        //For example: JavaRush Ltd.
+
+        String getCountryName();        //For example: Ukraine
+    }
+
+    public interface Contact {
+        String getName();               //For example: Ivanov, Ivan
+
+        String getPhoneNumber();        //For example: +38(050)123-45-67
+    }
+}
