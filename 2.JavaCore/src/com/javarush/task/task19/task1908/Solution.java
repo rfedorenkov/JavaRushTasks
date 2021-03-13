63064da902d4ebfca208e7e4dbf7443efd73dc47
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-03-13 13:29:50 +0300

    Java Core level 9 complete

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task19.task1908;
+
+import java.io.*;
+
+/**
+ * Выделяем числа
+ * Считать с консоли 2 имени файла.
+ * Вывести во второй файл все числа, которые есть в первом файле.
+ * Числа выводить через пробел.
+ * Закрыть потоки.
+ *
+ * Пример тела файла:
+ * 12 text var2 14 8ю 1
+ *
+ * Результат:
+ * 12 14 1
+ *
+ *
+ * Требования:
+ * 1. Программа должна считывать имена файлов с консоли (используй BufferedReader).
+ * 2. BufferedReader для считывания данных с консоли должен быть закрыт.
+ * 3. Программа должна считывать содержимое первого файла (используй BufferedReader
+ * c конструктором принимающим FileReader).
+ * 4. Поток чтения из файла (BufferedReader) должен быть закрыт.
+ * 5. Программа должна записывать во второй файл все числа, через пробел,
+ * из первого файла (используй BufferedWriter с конструктором FileWriter).
+ * 6. Поток записи в файл (BufferedWriter) должен быть закрыт.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String fileOne = br.readLine();
+            String fileTwo = br.readLine();
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(fileOne));
+                 BufferedWriter writer = new BufferedWriter(new FileWriter(fileTwo))) {
+                while (reader.ready()) {
+                    for (String s : reader.readLine().split(" ")) {
+                        try {
+                            Integer.parseInt(s);
+                            writer.write(s + " ");
+                        } catch (NumberFormatException ignored) {
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
