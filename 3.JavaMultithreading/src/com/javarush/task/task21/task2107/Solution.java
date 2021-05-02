a3ccd4788941b5423cdd5230126b061a23552511
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-05-02 09:54:43 +0300

    Java Multithreading level 1 complete

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
@@ -0,0 +1,82 @@
+package com.javarush.task.task21.task2107;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Глубокое клонирование карты
+ * Обеспечь возможность клонирования объекта класса Solution используя глубокое клонирование.
+ * Данные в карте users также должны быть клонированы.
+ * Не забудь о методах equals и hashCode для корректного добавления элементов типа User в HashMap.
+ *
+ *
+ * Требования:
+ * 1. Класс Solution должен поддерживать интерфейс Cloneable.
+ * 2. Класс User должен поддерживать интерфейс Cloneable.
+ * 3. В классе User должен быть корректно реализован метод clone.
+ * 4. В классе Solution должен быть корректно реализован метод clone.
+*/
+
+public class Solution implements Cloneable {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.users.put("Hubert", new User(172, "Hubert"));
+        solution.users.put("Zapp", new User(41, "Zapp"));
+        Solution clone;
+        try {
+            clone = (Solution) solution.clone();
+            System.out.println(solution);
+            System.out.println(clone);
+
+            System.out.println(solution.users);
+            System.out.println(clone.users);
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    protected Map<String, User> users = new LinkedHashMap<>();
+
+    public static class User implements Cloneable {
+        int age;
+        String name;
+
+        public User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof User)) return false;
+            User user = (User) o;
+            return age == user.age &&
+                    Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(age, name);
+        }
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        Solution clone = (Solution) super.clone();
+        Map<String, User> cloneUsers = new LinkedHashMap<>(users);
+        for (String s : clone.users.keySet()) {
+            cloneUsers.put(s, (User) clone.users.get(s).clone());
+        }
+        clone.users = cloneUsers;
+        return clone;
+    }
+}
