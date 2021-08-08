2f368617d70d7217ba0095ce63b5e02a501e69df
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-08-08 11:10:07 +0300

    Java Multithreading level 7 complete

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2709/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2709/Solution.java
@@ -38,7 +38,7 @@ package com.javarush.task.task27.task2709;
 */
 
 public class Solution {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String args[]) throws InterruptedException {
         TransferObject transferObject = new TransferObject();
         ProducerTask producerTask = new ProducerTask(transferObject);
         ConsumerTask consumerTask = new ConsumerTask(transferObject);
