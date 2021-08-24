873b3a82b99be3a2a27ac7cb71fc68f216710908
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-08-24 16:02:20 +0300

    Java Multithreading level 9 complete (Restaurant #11)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,4 +1,34 @@
 package com.javarush.task.task27.task2712.statistic;
 
-public class StatisticManager {
+import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+
+/**
+ * Класс менеджера статистики.
+ * Отвечает за регистрацию событий в хранилище.
+ * Класс является Singleton, поскольку относится ко всему ресторану.
+ */
+public enum StatisticManager {
+    INSTANCE;
+
+    /**
+     * Метод возвращает менеджера статистики.
+     *
+     * @return Менеджер статистики.
+     */
+    public static StatisticManager getInstance() {
+        return INSTANCE;
+    }
+
+    // подсчет статистики
+
+    // отображение статистики директору
+
+    /**
+     * Метод регистрирует событие в хранилище
+     *
+     * @param data Событие.
+     */
+    public void register(EventDataRow data) {
+
+    }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/event/EventDataRow.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task27.task2712.statistic.event;
+
+/**
+ * Интерфейс-маркер.
+ * Проверяется является ли переданный объект событием.
+ */
+public interface EventDataRow {
+}
