930652d7950e7f8c35ea172875d70815a2a65943
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-09-02 08:11:23 +0300

    Java Multithreading level 10 complete (2048 Game)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Класс запускает игру 2048.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Класс содержит игровую логику и хранит игровое поле.
+ */
+public class Model {
+}
