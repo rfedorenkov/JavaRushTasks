6df9f4c94d04e04f7783ba34bc7f216be57a1fc6
rfedorenkov <fedorenkovrostislav@gmail.com>
2021-09-02 08:36:51 +0300

    Java Multithreading level 10 complete (2048 Game)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -4,4 +4,22 @@ package com.javarush.task.task35.task3513;
  * Класс содержит игровую логику и хранит игровое поле.
  */
 public class Model {
+    // Ширина игрового поля
+    private static final int FIELD_WIDTH = 4;
+    // Двумерный массив игрового поля состоящий из плиток (Tile)
+    private Tile[][] gameTiles;
+
+    /**
+     * Конструктор объекта.
+     * Создает игровое игровое поле по значению FIELD_WIDTH х FIELD_WIDTH.
+     * Инициализирует двумерный массив gameTiles новыми объектами Tile.
+     */
+    public Model() {
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[i].length; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
