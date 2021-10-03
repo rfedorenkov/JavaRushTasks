457714bc4f90bf29c84b3d73aabc383ca058b196
Rostislav Fedorenkov <fedorenkovrostislav@gmail.com>
2021-10-03 11:44:35 +0300

    Java Collections level 2 complete (Tetris)

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -4,4 +4,46 @@ package com.javarush.task.task22.task2213;
  * Класс фигуры.
  */
 public class Figure {
+    // Координаты
+    private int x;
+    private int y;
+
+    // Форма фигуры (состоящий из единиц и нулей)
+    private int[][] matrix = new int[3][3];
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param matrix
+     */
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+
+    /**
+     * Геттер координаты x.
+     * @return Координату x.
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Геттер координаты y.
+     * @return Координату y.
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Геттер формы фигуры.
+     * @return Матрицу matrix.
+     */
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
