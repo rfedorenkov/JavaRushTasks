8c9077bb163fd26da504283f3ded3cb52320b122
Rostislav Fedorenkov <fedorenkovrostislav@gmail.com>
2021-10-09 19:25:03 +0300

    Java Collections level 3 complete (Space)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,28 +1,27 @@
 package com.javarush.task.task25.task2515;
 
 /**
- * Класс ракеты.
+ * Класс для объектов-ракета
  */
 public class Rocket extends BaseObject {
 
-    /**
-     * Конструктор объекта.
-     * При создании устанавливается параметр isAlive = true.
-     *
-     * @param x      Координата 'x'
-     * @param y      Координата 'y'
-     */
     public Rocket(double x, double y) {
         super(x, y, 1);
     }
 
-    @Override
-    public void move() {
-        y--;
-    }
-
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
     @Override
     public void draw(Canvas canvas) {
         canvas.setPoint(x, y, 'R');
+    }
+
+    /**
+     * Двигаем себя вверх на один ход.
+     */
+    @Override
+    public void move() {
+        y--;
     }
 }
