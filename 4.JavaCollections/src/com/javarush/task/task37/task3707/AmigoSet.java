01fabe2eacc4bc82ee9b0ab8ab318171062e940d
Rostislav Fedorenkov <fedorenkovrostislav@gmail.com>
2021-10-12 17:10:39 +0300

    Java Collections level 4 complete (AmigoSet)

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task37.task3707;
+
+import java.io.Serializable;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Базовый класс Set.
+ */
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
