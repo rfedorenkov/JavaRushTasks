de96bc10a606d5db5b23e6cfcbcead3e0b07333b
Rostislav Fedorenkov <rostislav.fedorenkov@gmail.com>
2021-10-15 22:02:18 +0300

    Java Collections level 5 complete (Factories) Add FemaleFactory

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,12 +1,18 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.female.FemaleFactory;
 import com.javarush.task.task37.task3702.male.MaleFactory;
 
 public class Solution {
     public static void main(String[] args) {
-        MaleFactory factory = new MaleFactory();
-        System.out.println(factory.getPerson(99));
-        System.out.println(factory.getPerson(4));
-        System.out.println(factory.getPerson(15));
+        MaleFactory maleFactory = new MaleFactory();
+        System.out.println(maleFactory.getPerson(99));
+        System.out.println(maleFactory.getPerson(4));
+        System.out.println(maleFactory.getPerson(15));
+
+        FemaleFactory femaleFactory = new FemaleFactory();
+        System.out.println(femaleFactory.getPerson(99));
+        System.out.println(femaleFactory.getPerson(4));
+        System.out.println(femaleFactory.getPerson(15));
     }
 }
