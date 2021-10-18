ee6f217ea8cabafd5eb0861534b7d76dcc27a63b
Rostislav Fedorenkov <rostislav.fedorenkov@gmail.com>
2021-10-18 21:32:40 +0300

    Java Collections level 6 complete (Shortener) Task #3

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 /**
  * Класс, который может для любой строки вернуть некий
  * уникальный идентификатор и наоборот, по ранее полученному
@@ -7,22 +9,43 @@ package com.javarush.task.task33.task3310;
  */
 public class Shortener {
 
+    // Последнее значение идентификатора
+    private volatile Long lastId = 0L;
+
+    // Стратегия хранения данных
+    private StorageStrategy storageStrategy;
+
+    /**
+     * Конструктор класса.
+     *
+     * @param storageStrategy Стратегия хранения данных.
+     */
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
+    }
+
     /**
      * Метод возвращает идентификатор 'id' для заданной стройки.
      *
      * @param string Заданная строка.
      * @return Идентификатор 'id'.
      */
-    public Long getId(String string) {
-        return -1L;
+    public synchronized Long getId(String string) {
+        if (storageStrategy.containsValue(string)) {
+            return storageStrategy.getKey(string);
+        }
+        lastId++;
+        storageStrategy.put(lastId, string);
+        return lastId;
     }
 
     /**
      * Метод возвращает строку для заданного идентификатора.
+     *
      * @param id Идентификатор.
      * @return Строку или null, если передан неверный идентификатор.
      */
-    public String getString(Long id) {
-        return null;
+    public synchronized String getString(Long id) {
+        return storageStrategy.getValue(id);
     }
 }
