9382d6d5741ef3ebfc16fb3082f8ec50931d912d
Rostislav Fedorenkov <rostislav.fedorenkov@gmail.com>
2021-10-21 10:09:27 +0300

    Java Collections level 6 complete (Shortener) Task #11

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -57,17 +57,18 @@ public class Solution {
         long result = new Date().getTime() - start.getTime();
         Helper.printMessage(String.valueOf(result));
 
-//        start = new Date();
-//        Set<String> testKeys = getStrings(shortener, testIds);
-//        result = new Date().getTime() - start.getTime();
-//        Helper.printMessage(String.valueOf(result));
+        start = new Date();
+        Set<String> testKeys = getStrings(shortener, testIds);
+        result = new Date().getTime() - start.getTime();
+        Helper.printMessage(String.valueOf(result));
 
-//        Helper.printMessage(testSet.containsAll(testKeys) ? "Тест пройден." : "Тест не пройден.");
+        Helper.printMessage(testSet.containsAll(testKeys) ? "Тест пройден." : "Тест не пройден.");
     }
 
     public static void main(String[] args) {
         testStrategy(new HashMapStorageStrategy(), 10_000);
         testStrategy(new OurHashMapStorageStrategy(), 10_000);
         testStrategy(new FileStorageStrategy(), 100);
+        testStrategy(new OurHashBiMapStorageStrategy(), 100_000);
     }
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/OurHashBiMapStorageStrategy.java
@@ -0,0 +1,64 @@
+package com.javarush.task.task33.task3310.strategy;
+
+import java.util.HashMap;
+
+public class OurHashBiMapStorageStrategy implements StorageStrategy {
+    private HashMap<Long, String> k2v = new HashMap<>();
+    private HashMap<String, Long> v2k = new HashMap<>();
+
+    /**
+     * Метод должен вернуть true, если хранилище содержит переданный ключ.
+     *
+     * @param key Ключ.
+     * @return true - если содержит ключ, в противном - false.
+     */
+    @Override
+    public boolean containsKey(Long key) {
+        return k2v.containsKey(key);
+    }
+
+    /**
+     * Метод должен вернуть true, если хранилище содержит переданное значение.
+     *
+     * @param value Значение.
+     * @return true - если содержит значение, в противном - false.
+     */
+    @Override
+    public boolean containsValue(String value) {
+        return v2k.containsKey(value);
+    }
+
+    /**
+     * Добавляет в хранилище новую пару ключ - значение.
+     *
+     * @param key   Ключ.
+     * @param value Значение.
+     */
+    @Override
+    public void put(Long key, String value) {
+        k2v.put(key, value);
+        v2k.put(value, key);
+    }
+
+    /**
+     * Возвращает ключ для переданного значения.
+     *
+     * @param value Значение.
+     * @return Ключ.
+     */
+    @Override
+    public Long getKey(String value) {
+        return v2k.get(value);
+    }
+
+    /**
+     * Возвращает значение для переданного ключа.
+     *
+     * @param key Ключ.
+     * @return Значение.
+     */
+    @Override
+    public String getValue(Long key) {
+        return k2v.get(key);
+    }
+}
