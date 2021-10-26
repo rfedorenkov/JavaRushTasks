6966fc391d5db1feab46ebe4b97edc90020366e7
Rostislav Fedorenkov <rostislav.fedorenkov@gmail.com>
2021-10-26 20:57:30 +0300

    Java Collections level 8 complete (Aggregator)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,11 +14,17 @@ public class HHStrategy implements Strategy {
     //https://grc.ua/search/vacancy?text=java+Kiev&page=3
     //https://hh.ru/search/vacancy?text=java+Kiev&page=3
 
-    private static final String URL_FORMAT = "https://grc.ua/search/vacancy?text=java+%s&page=%s";
+    private static final String URL_FORMAT = "https://grc.ua/search/vacancy?text=java+%s&page=%d";
     // String.format(URL_FORMAT, "Kiev", 3);
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
+        try {
+            Document document = Jsoup.connect(String.format(URL_FORMAT, "Moscow", 0)).get();
+//            System.out.println(document.html());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return Collections.emptyList();
     }
 
