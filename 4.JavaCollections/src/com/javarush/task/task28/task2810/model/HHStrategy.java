eb1912619fbbac26ca1bf23e8c8837c8665075c0
Rostislav Fedorenkov <rostislav.fedorenkov@gmail.com>
2021-10-26 19:55:18 +0300

    Java Collections level 8 complete (Aggregator)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -2,4 +2,10 @@ package com.javarush.task.task28.task2810.model;
 
 //Этот класс будет реализовывать конкретную стратегию работы с сайтом поиска работы (https://grc.ua/ и https://hh.ru/).
 public class HHStrategy implements Strategy {
+    //https://grc.ua/search/vacancy?text=java+ADDITIONAL_VALUE&page=PAGE_VALUE
+    //https://grc.ua/search/vacancy?text=java+Kiev&page=3
+    //https://hh.ru/search/vacancy?text=java+Kiev&page=3
+
+    private static final String URL_FORMAT = "https://grc.ua/search/vacancy?text=java+%s&page=%s";
+    // String.format(URL_FORMAT, "Kiev", 3);
 }
