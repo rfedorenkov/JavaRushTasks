822c58f136477d9c53bd5a0d91a51cffea265fa2
Rostislav Fedorenkov <rostislav.fedorenkov@gmail.com>
2021-10-27 22:24:16 +0300

    Java Collections level 8 complete (Aggregator)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -3,14 +3,22 @@ package com.javarush.task.task28.task2810.view;
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class HtmlView implements View {
+    private final String filePath = "./4.JavaCollections/src/" + this.getClass().getPackage().getName().replace('.', '/') + "/vacancies.html";
     private Controller controller;
 
     @Override
     public void update(List<Vacancy> vacancies) {
-        System.out.println(vacancies.size());
+        String fileContent = getUpdatedFileContent(vacancies);
+        updateFile(fileContent);
     }
 
     @Override
@@ -20,5 +28,17 @@ public class HtmlView implements View {
 
     public void userCitySelectEmulationMethod() {
         controller.onCitySelect("Odessa");
+    }
+
+    private String getUpdatedFileContent(List<Vacancy> vacancies) {
+        return null;
+    }
+
+    private void updateFile(String text) {
+        try (FileWriter fis = new FileWriter(filePath)) {
+            fis.write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
