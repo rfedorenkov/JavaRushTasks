d7f72519b50f503234df153cd939d944c374e87b
Rostislav Fedorenkov <rostislav.fedorenkov@gmail.com>
2021-10-27 12:55:05 +0300

    Java Collections level 8 complete (Aggregator)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -5,8 +5,6 @@ import com.javarush.task.task28.task2810.model.Provider;
 
 public class Aggregator {
     public static void main(String[] args) {
-        Provider provider = new Provider(new HHStrategy());
-        Controller controller = new Controller(provider);
-        controller.scan();
+
     }
 }
