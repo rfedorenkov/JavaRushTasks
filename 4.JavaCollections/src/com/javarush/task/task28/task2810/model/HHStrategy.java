1d021f46267cf095c6db752b3bc6af09b47b5884
Rostislav Fedorenkov <rostislav.fedorenkov@gmail.com>
2021-10-28 11:14:40 +0300

    Java Collections level 8 complete (Aggregator)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -9,15 +9,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-//Этот класс будет реализовывать конкретную стратегию работы с сайтом поиска работы (https://grc.ua/ и https://hh.ru/).
 public class HHStrategy implements Strategy {
-    //https://grc.ua/search/vacancy?text=java+ADDITIONAL_VALUE&page=PAGE_VALUE
-    //https://grc.ua/search/vacancy?text=java+Kiev&page=3
-    //https://hh.ru/search/vacancy?text=java+Kiev&page=3
 
     private static final String URL_FORMAT = "https://grc.ua/search/vacancy?text=java+%s&page=%d";
     private static final String URL_FORMAT2 = "https://javarush.ru/testdata/big28data.html"; // Hash page
-    // String.format(URL_FORMAT, "Kiev", 3);
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -2,13 +2,15 @@ package com.javarush.task.task28.task2810.view;
 
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
 
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class HtmlView implements View {
@@ -30,9 +32,49 @@ public class HtmlView implements View {
         controller.onCitySelect("Odessa");
     }
 
-    private String getUpdatedFileContent(List<Vacancy> vacancies) {
-        return null;
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
     }
+
+    private String getUpdatedFileContent(List<Vacancy> vacancies) {
+
+        try {
+            Document doc = getDocument();
+            Elements templateHidden = doc.getElementsByClass("template");
+            Element template = templateHidden.clone().removeAttr("style").removeClass("template").get(0);
+
+            Elements prevVacancies = doc.getElementsByClass("vacancy");
+
+            prevVacancies.stream()
+                    .filter(redundant -> !redundant.hasClass("template"))
+                    .forEach(Node::remove);
+
+            vacancies.forEach(vacancy -> {
+                Element element = template.clone();
+
+                Element url = element.getElementsByAttribute("href").get(0);
+                url.appendText(vacancy.getTitle());
+                url.attr("href", vacancy.getUrl());
+
+                Element city = element.getElementsByClass("city").get(0);
+                city.appendText(vacancy.getCity());
+
+                Element companyName = element.getElementsByClass("companyName").get(0);
+                companyName.appendText(vacancy.getCompanyName());
+
+                Element salary = element.getElementsByClass("salary").get(0);
+                salary.appendText(vacancy.getSalary());
+
+                templateHidden.before(element.outerHtml());
+            });
+
+            return doc.html();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Some exception occurred";
+        }
+    }
+
 
     private void updateFile(String text) {
         try (FileWriter fis = new FileWriter(filePath)) {
@@ -42,3 +84,35 @@ public class HtmlView implements View {
         }
     }
 }
+
+
+//3. Удали все добавленные ранее вакансии. У них единственный класс "vacancy".
+//В файле backup.html это одна вакансия - Junior Java Developer.
+//Нужно удалить все теги tr, у которых class="vacancy".
+//Но тег tr, у которого class="vacancy template", не удаляй.
+//Используй метод remove.
+//
+//4. В цикле для каждой вакансии полученной как параметр метода:
+//4.1. склонируй шаблон тега, полученного в п.2. Метод clone.
+//4.2. получи элемент, у которого есть класс "city". Запиши в него название города из вакансии.
+//4.3. получи элемент, у которого есть класс "companyName". Запиши в него название компании из вакансии.
+//4.4. получи элемент, у которого есть класс "salary". Запиши в него зарплату из вакансии.
+//4.5. получи элемент-ссылку с тегом a. Запиши в него название вакансии(title). Установи реальную ссылку на вакансию вместо href="url".
+//4.6. добавь outerHtml элемента, в который ты записывал данные вакансии,
+//непосредственно перед шаблоном <tr class="vacancy template" style="display: none">
+//
+//5. Верни html код всего документа в качестве результата работы метода.
+//
+//6. В случае возникновения исключения, выведи его стек-трейс и верни строку "Some exception occurred".
+//
+//7. Запусти приложение, убедись, что все вакансии пишутся в файл vacancies.html.
+//
+
+//3. Получи элемент, у которого есть класс template. Сделай копию этого объекта, удали из нее атрибут "style" и класс "template".
+//4. Удали из страницы все добавленные ранее вакансии с классом "vacancy". Элемент с классом "vacancy template" должен остаться.
+//5. Перед объектом template для каждой вакансии добавь на страницу отдельный html-элемент, используя копию template. Верни html-код всей страницы в качестве результата работы метода.
+//6. Для каждой вакансии должен быть корректно заполнен элемент-ссылка с названием вакансии(title) и http-ссылкой на нее(href).
+//7. Для каждой вакансии должен быть корректно заполнен элемент с классом "city".
+//8. Для каждой вакансии должен быть корректно заполнен элемент с классом "companyName".
+//9. Для каждой вакансии должен быть корректно заполнен элемент с классом "salary".
+//10. В случае возникновения исключения, выведи его стек-трейс и верни строку "Some exception occurred".
