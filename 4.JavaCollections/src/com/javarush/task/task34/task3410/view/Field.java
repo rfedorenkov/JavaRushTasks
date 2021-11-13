88cb281bec3bfed017b144ba86c28a6647ce783b
Rostislav Fedorenkov <rostislav.fedorenkov@gmail.com>
2021-11-13 16:13:50 +0300

    Java Collections level 10 complete (Sokoban #9)

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task34.task3410.view;
 
+import com.javarush.task.task34.task3410.controller.EventListener;
 import com.javarush.task.task34.task3410.model.Box;
 import com.javarush.task.task34.task3410.model.Home;
 import com.javarush.task.task34.task3410.model.Player;
@@ -9,9 +10,14 @@ import java.awt.*;
 
 public class Field extends JPanel {
     private View view;
+    private EventListener eventListener;
 
     public Field(View view) {
         this.view = view;
+    }
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
     }
 
     @Override
